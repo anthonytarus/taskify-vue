9b143125a8f2a632e85e4c69e6d0ad4cd67f398c
anthony <44896838+anthonytarus@users.noreply.github.com>
2022-10-19 17:12:49 +0300

    Initial commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    @toggle-reminder="$emit('toggle-reminder', task.id)"
+    :key="task.id"
+    v-for="task in tasks"
+  >
+    <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
